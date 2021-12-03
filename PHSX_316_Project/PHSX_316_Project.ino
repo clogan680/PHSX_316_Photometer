a97949a5f75ee2bcce13745f83956c4ab50d22f1
unknown <clogan680@gmail.com>
2021-12-02 22:34:35 -0800

    staging changes for power calcs

--- PHSX_316_Project/PHSX_316_Project.ino
+++ PHSX_316_Project/PHSX_316_Project.ino
@@ -1,7 +1,23 @@
 #include <LiquidCrystal.h>
 
-int light;
-float volts;
+int light1;
+int light2;
+
+float volts1;
+float volts2;
+
+float pr_5549;
+float pr_5506;
+
+float R_fixed;
+float V_supply;
+
+float Current1;
+float Current2;
+
+float power1;
+float power2;
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
@@ -13,18 +29,24 @@ void setup() {
 
 void loop() {
 
-  light = analogRead(A0);
-  volts = light * .0049;
-  Serial.println(volts);
+  light1 = analogRead(A0);
+  light2 = analogRead(A5);
+
+  volts1 = light1 * .0049;
+  volts2 = light2 * .0049;
+  //  Serial.println(volts1);
 
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Reading Volts");
-  lcd.setCursor(0, 1);
-  lcd.print("Value : ");
-  lcd.setCursor(10, 1);
-  lcd.print(volts);
+  lcd.setCursor(2, 0);
+  lcd.print("LDR A0:");
+  lcd.setCursor(10, 0);
+  lcd.print(volts1);
 
-  delay(300);
+  lcd.setCursor(2, 1);
+  lcd.print("LDR A5:");
+  lcd.setCursor(10, 1);
+  lcd.print(volts2);
+
+  delay(100);
 
 }
