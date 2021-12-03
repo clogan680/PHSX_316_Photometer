8e14b11e431031c63dcfb612b7f5cbe0bead0c6b
unknown <clogan680@gmail.com>
2021-12-03 12:32:47 -0800

    calculations finished

--- PHSX_316_Project/PHSX_316_Project.ino
+++ PHSX_316_Project/PHSX_316_Project.ino
@@ -9,11 +9,12 @@ float volts2;
 float pr_5549;
 float pr_5506;
 
-float R_fixed;
-float V_supply;
+float r_fixed = 10000;
+float v_supply_5 = 5;
+float v_supply_3_3 = 3.333;
 
-float Current1;
-float Current2;
+float current1;
+float current2;
 
 float power1;
 float power2;
@@ -34,18 +35,26 @@ void loop() {
 
   volts1 = light1 * .0049;
   volts2 = light2 * .0049;
-  //  Serial.println(volts1);
+
+  pr_5549 = v_supply_5 * r_fixed / volts1 - r_fixed;
+  pr_5506 = v_supply_3_3 * r_fixed / volts2 - r_fixed;
+
+  current1 = volts1 / pr_5549;
+  current2 = volts2 / pr_5506;
+
+  power1 = volts1 * current1;
+  power2 = volts2 * current2;
 
   lcd.clear();
-  lcd.setCursor(2, 0);
-  lcd.print("LDR A0:");
-  lcd.setCursor(10, 0);
-  lcd.print(volts1);
+  lcd.setCursor(0, 0);
+  lcd.print("Watts1:");
+  lcd.setCursor(8, 0);
+  lcd.print(power1, 6);
 
-  lcd.setCursor(2, 1);
-  lcd.print("LDR A5:");
-  lcd.setCursor(10, 1);
-  lcd.print(volts2);
+  lcd.setCursor(0, 1);
+  lcd.print("Watts2:");
+  lcd.setCursor(8, 1);
+  lcd.print(power2, 6);
 
   delay(100);
 
